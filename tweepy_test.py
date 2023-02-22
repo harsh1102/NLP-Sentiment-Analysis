c4f1005fcfd562cf4bbd56a642ab68475dd57472
harsh1102 <harshmuniwala33@gmail.com>
2023-02-22 02:39:51 -0600

    Testing data and stop words

--- tweepy_test.py
+++ tweepy_test.py
@@ -1,48 +1,56 @@
 import tweepy
 import pandas as pd
-import csv
-import json
 import config
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-
-# Replace your bearer token below
-# client = Twarc2(bearer_token="XXXXX")
-
-# Twitter authentication
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)   
-# auth.set_access_token(access_key, access_secret) 
-# auth = tweepy.OAuth2BearerHandler(bearer_token)
+# Twitter authentication 
+auth = tweepy.OAuth2BearerHandler(bearer_token=config.bearer_token)
 
   
 # Creating an API object 
-# api = tweepy.API(auth)
+api = tweepy.API(auth)
 client = tweepy.Client(bearer_token=config.bearer_token)
+
+# nltk.download('stopwords')
+# nltk.download('punkt')
+stop_words = set(stopwords.words('english'))
 
 
 
 def main():
-    list=[]
-    query = '#SidKiaraWedding -is:retweet lang:en'
-    tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=10)
-    # hashtag_tweets = tweepy.Cursor(api.search_tweets, q="#VaccinationDrive", tweet_mode='extended').items(5)
+    # query = '#SidKiaraWedding -is:retweet lang:en'
+    query = 'FastAndFurious'
+    i = 0
+    # with open(file_name, 'a+', encoding="utf-8") as filehandle:
+    for tweet in tweepy.Paginator(client.search_recent_tweets, query=query, 
+                                  tweet_fields=['context_annotations', 'created_at'], max_results=10).flatten(limit=1):
+        i = i +1
+        print(tweet.text)
+        word_tokens = word_tokenize(tweet.text)
+        filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+        print(filtered_sentence)
+        file_name = "tweets_"+str(i)+".txt"
+        File_object = open(file_name, "a+", encoding="utf-8")
+        File_object.write('%s\n' % tweet.text)
 
-    # out = csv.writer(open("myfile.csv","w"), delimiter=',',quoting=csv.QUOTE_ALL)
-    for tweet in tweets.data:
-        print(tweets.data)
-        text = tweet.text
-        if len(tweet.context_annotations) > 0:
-            # print(len(tweet.context_annotations))
-            refined_tweet = {
-                'text' : text,
-                # 'id' : tweet.context_annotations[0]['domain']['id'],
-                # 'name' : tweet.context_annotations[0]['domain']['name'],
-                'context' : tweet.context_annotations
-            }
-            list.append(refined_tweet)
-            # print(text)
-            # print(tweet.context_annotations)
-            # print(list)
-            break
+    # for tweet in tweets.data:
+    #     print(tweets.data)
+    #     text = tweet.text
+    #     if len(tweet.context_annotations) > 0:
+    #         # print(len(tweet.context_annotations))
+    #         refined_tweet = {
+    #             'text' : text,
+    #             # 'id' : tweet.context_annotations[0]['domain']['id'],
+    #             # 'name' : tweet.context_annotations[0]['domain']['name'],
+    #             'context' : tweet.context_annotations
+    #         }
+    #         list.append(refined_tweet)
+    #         # print(text)
+    #         # print(tweet.context_annotations)
+    # print(list)
+            # break
             # print(tweet.context_annotations[0]['domain']['id'])
             # jdump = json.dumps(tweet.context_annotations)
             # print(tweet.context_annotations[1])
@@ -50,6 +58,22 @@ def main():
     # df = pd.DataFrame(list)
     # df.to_csv('refined_tweets.csv')
 
+
+###################### Elevated access needed #########################
+    # searched_tweets = [status for status in tweepy.Cursor(api.search_tweets, q=query).items(max_tweets)]
+    # for tweet in searched_tweets:
+    #     username = tweet.user.screen_name
+    #     description = tweet.user.description
+    #     location = tweet.user.location
+    #     following = tweet.user.friends_count
+    #     followers = tweet.user.followers_count
+    #     totaltweets = tweet.user.statuses_count
+    #     retweetcount = tweet.retweet_count
+    #     hashtags = tweet.entities['hashtags']
+    #     print(tweet)
+    #     break;
+
+#############################################################################
 
 
 if __name__ == "__main__":
