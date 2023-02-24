c76a59ae885a55d43b79d2cb7ceac256101c46d2
harsh1102 <harshmuniwala33@gmail.com>
2023-02-24 00:29:23 -0600

    Data prep

--- tweepy_test.py
+++ tweepy_test.py
@@ -1,9 +1,11 @@
 import tweepy
 import pandas as pd
 import config
-import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import string
+string.punctuation
+import preprocessor as prep
 
 # Twitter authentication 
 auth = tweepy.OAuth2BearerHandler(bearer_token=config.bearer_token)
@@ -21,60 +23,43 @@ stop_words = set(stopwords.words('english'))
 
 def main():
     # query = '#SidKiaraWedding -is:retweet lang:en'
-    query = 'FastAndFurious'
+    query = 'FastAndFurious lang:en'
+    # query = 'Dhoni lang:en'
     i = 0
     # with open(file_name, 'a+', encoding="utf-8") as filehandle:
     for tweet in tweepy.Paginator(client.search_recent_tweets, query=query, 
                                   tweet_fields=['context_annotations', 'created_at'], max_results=10).flatten(limit=1):
         i = i +1
+        # test_text = '''RT @SushantNMehta: If MS Dhoni wins #IPL2023 would he go down in history as the best cricketing mind of all time? 
+#MSDhoni #SushantMehta'''
+        print("------------Original Text --------------------")
         print(tweet.text)
-        word_tokens = word_tokenize(tweet.text)
-        filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
-        print(filtered_sentence)
-        file_name = "tweets_"+str(i)+".txt"
-        File_object = open(file_name, "a+", encoding="utf-8")
-        File_object.write('%s\n' % tweet.text)
+        print("------------After text cleaning---------------")
+        cleaned_text = text_cleaning(tweet.text)
+        print(cleaned_text)
+        print("----------After removing punctuation-----------")
+        punctuationfree_text = punctuation_remove(cleaned_text)
+        print(punctuationfree_text)
+        print("---------After removing stopwords--------------")
+        stopwords_removed = stopwords_remove(punctuationfree_text)
+        print(stopwords_removed)
 
-    # for tweet in tweets.data:
-    #     print(tweets.data)
-    #     text = tweet.text
-    #     if len(tweet.context_annotations) > 0:
-    #         # print(len(tweet.context_annotations))
-    #         refined_tweet = {
-    #             'text' : text,
-    #             # 'id' : tweet.context_annotations[0]['domain']['id'],
-    #             # 'name' : tweet.context_annotations[0]['domain']['name'],
-    #             'context' : tweet.context_annotations
-    #         }
-    #         list.append(refined_tweet)
-    #         # print(text)
-    #         # print(tweet.context_annotations)
-    # print(list)
-            # break
-            # print(tweet.context_annotations[0]['domain']['id'])
-            # jdump = json.dumps(tweet.context_annotations)
-            # print(tweet.context_annotations[1])
-            # out.writerow(tweet.context_annotations)
-    # df = pd.DataFrame(list)
-    # df.to_csv('refined_tweets.csv')
+        # file_name = "tweets_"+str(i)+".txt"
+        # File_object = open(file_name, "a+", encoding="utf-8")
+        # File_object.write('%s\n' % tweet.text)
 
 
-###################### Elevated access needed #########################
-    # searched_tweets = [status for status in tweepy.Cursor(api.search_tweets, q=query).items(max_tweets)]
-    # for tweet in searched_tweets:
-    #     username = tweet.user.screen_name
-    #     description = tweet.user.description
-    #     location = tweet.user.location
-    #     following = tweet.user.friends_count
-    #     followers = tweet.user.followers_count
-    #     totaltweets = tweet.user.statuses_count
-    #     retweetcount = tweet.retweet_count
-    #     hashtags = tweet.entities['hashtags']
-    #     print(tweet)
-    #     break;
+def punctuation_remove(row_text):
+    punctuationfree="".join([i for i in row_text if i not in string.punctuation])
+    return punctuationfree.lower()
 
-#############################################################################
+def text_cleaning(row_text):
+    return prep.clean(row_text)
 
+def stopwords_remove(row_text):
+    word_tokens = word_tokenize(row_text)
+    filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+    return filtered_sentence
 
 if __name__ == "__main__":
     main()
